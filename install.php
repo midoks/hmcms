93e3a8ddf5f46c2ada24a6303a6b6dfe33d12d5a
Mr Chen <midoks@163.com>
2023-12-19 00:37:52 +0800

    Update install.php

--- install.php
+++ install.php
@@ -12,10 +12,19 @@
 // [ 应用入口文件 ]
 namespace think;
 
+if(is_file('./app/data/install/install.lock')) {
+	echo '如需重新安装请删除【To re install, please remove】 >>> /app/data/install/install.lock';
+	exit;
+}
+
+if(!is_writable('./runtime')) {
+	echo '请开启[runtime]目录的读写权限【Please turn on the read and write permissions of the [runtime] folder】';
+	exit;
+}
+
 require __DIR__ . '/vendor/autoload.php';
 
 // 执行HTTP应用并响应
-// 
 $http = (new App())->debug()->http;
 $response = $http->name('install')->run();
 $response->send();
