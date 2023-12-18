047a77a02fbe802cdbe8a10651585858c61fee01
Mr Chen <midoks@163.com>
2023-12-19 02:02:10 +0800

    update

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -5,7 +5,6 @@ namespace app\admin\controller;
 use app\BaseController;
 use think\facade\View;
 use think\facade\Db;
-use think\DbManager;
 
 
 class Index extends BaseController
@@ -14,13 +13,13 @@ class Index extends BaseController
     private function fetch($tpl='index/index')
     {
         $root_path = $this->app->getRootPath();
-        $install_path = $root_path.'/app/install/view';
+        $install_path = $root_path.'/app/admin/view';
         return View::fetch($install_path.'/'.$tpl.'.html');
     }
 
-    public function index($step = 0)
+    public function index()
     {
-        return 'ss';
+        return $this->fetch('index/index');
     }
 
 }
