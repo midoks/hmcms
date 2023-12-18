425d4c875b019b06a828ad0971ea0185f19d459c
Mr Chen <midoks@163.com>
2023-12-19 03:51:29 +0800

    update

--- app/common/controller/Admin.php
+++ app/common/controller/Admin.php
@@ -9,5 +9,11 @@ use think\facade\Db;
 class Admin extends Base
 {
 
+	 // 初始化
+    protected function initialize()
+    {
+    	View::assign("version",time());
+    }
+
 
 }
--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -2,13 +2,13 @@
 
 namespace app\home\controller;
 
-use app\BaseController;
+use app\common\controller\Base;
 use think\facade\View;
 use think\facade\Db;
 use think\DbManager;
 
 
-class Index extends BaseController
+class Index extends Base
 {
 
     public function index($step = 0)
