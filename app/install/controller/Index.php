bc25f86d345d3e7271b8dfcf5309671835696f5d
Mr Chen <midoks@163.com>
2023-12-18 18:53:47 +0800

    update

--- app/install/controller/Index.php
+++ app/install/controller/Index.php
@@ -3,18 +3,23 @@
 namespace app\install\controller;
 
 use app\BaseController;
+use think\facade\View;
 
 class Index extends BaseController
 {
-    public function index()
+
+    private function fech($tpl='index/index')
     {
         $root_path = $this->app->getRootPath();
+        $install_path = $root_path.'/app/install/view';
 
-        var_dump($root_path);
+        // var_dump($install_path.'/'.$tpl);
+        return View::fetch($install_path.'/'.$tpl.'.html');
+    }
 
-
-
-        return $this->fetch('install@/index/index');
+    public function index()
+    {
+        return $this->fech('index/index');
     }
 
     public function hello($name = 'ThinkPHP8')
