7588383f28bdff9c7ded961e2bed4ac2d79af0f3
Mr Chen <midoks@163.com>
2024-01-04 02:40:57 +0800

    update

--- app/install/sql/initdata.sql
+++ app/install/sql/initdata.sql
@@ -1,5 +1,5 @@
 
 INSERT INTO `hm_admin_menu` (`id`, `name`, `title`, `status`, `remark`, `sort`, `type`, `pid`, `level`) VALUES
 (1, '首页', '首页', 0, '首页', 1, 'nav', 0, 1),
-(2, '系统', '系统', 0, '系统', 1, 'nav', 0, 1);
+(2, '系统', '系统', 0, '系统', 2, 'nav', 0, 1);
 
