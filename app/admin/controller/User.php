3ed411093bc13320a3ce02b55da18f66c09fbefd
Mr Chen <midoks@163.com>
2024-02-26 22:08:07 +0800

    update

--- app/admin/controller/User.php
+++ app/admin/controller/User.php
@@ -82,6 +82,60 @@ class User extends AdminBase
         return $this->layuiJson(0, 'ok', $list, $count);
     }
 
+    public function save(){
+        $id = $this->request->post('id');
+
+        $data = [];
+        $data['cid'] = $this->request->post('cid');
+        $data['sid'] = $this->request->post('sid');
+        $data['signing'] = $this->request->post('signing');
+        $data['name'] = $this->request->post('name');
+        $data['nick'] = $this->request->post('nick');
+        $data['tel'] = $this->request->post('tel');
+        $data['qq'] = $this->request->post('qq');
+        $data['email'] = $this->request->post('email');
+        $data['city'] = $this->request->post('city');
+        $data['sex'] = $this->request->post('sex');
+        $data['text'] = $this->request->post('text');
+        $data['rz_msg'] = $this->request->post('rz_msg');
+        $data['vip'] = $this->request->post('vip');
+        $data['cion'] = $this->request->post('cion');
+        $data['rmb'] = $this->request->post('rmb');
+        $data['ticket'] = $this->request->post('ticket');
+        $data['realname'] = $this->request->post('realname');
+        $data['idcard'] = $this->request->post('idcard');
+        $data['bank'] = $this->request->post('bank');
+        $data['card'] = $this->request->post('card');
+        $data['bankcity'] = $this->request->post('bankcity');
+
+
+        if(empty($data['name'])){
+            return $this->returnJson(-2, '登陆账号不能为空~！');
+        }
+
+        if($id == 0 && empty($pass)){
+            return $this->returnJson(-2, '登陆密码不能为空~！');
+        }
+        
+        $pass = $this->request->post('pass');
+        if(!empty($pass)){
+            $data['pass'] = md5($pass);
+        }
+        
+        $m = $this->model('User');
+        $r = $m->dataSave($data, $id);
+
+
+        $msg_head = $id > 0 ? '更新' : '添加';
+        if ($r){
+            return $this->returnJson(1, $msg_head.'成功!');
+        } else {
+            return $this->returnJson(-2, $msg_head.'失败!');
+        }
+
+
+    }
+
 
     public function del(){
         $id = $this->request->param('id');
